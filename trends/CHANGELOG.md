432f8ac1573aa816be1e039a2928c23946b211cd
Petro Kashlikov <42810169+petrokashlikov@users.noreply.github.com>
2021-05-14 05:39:42 -0400

    Initial commit for Trends Dashboard release (#33)
    Co-authored-by: Petro Kashlikov <pkashlik@amazon.com>

---
+++ trends/CHANGELOG.md
@@ -0,0 +1,4 @@
+# What's new in Trends Dashboard
+
+## Trends Dashboard - v5.0
+* Initial release
---
+++ trends/view-templates/01_payer_account_name_map_sp.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW payer_account_name_map AS
+SELECT
+  "account_id"
+, "account_name" "payer_account_name"
+FROM
+  aws_accounts
+WHERE ("account_id" IN (SELECT "parent_account_id"
+FROM
+  aws_accounts
+WHERE ("parent_account_id" = "account_id")
+))
