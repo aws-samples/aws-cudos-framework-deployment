3d3a585f2871ce9c1cf9dafa601417cde0899e1b
Iakov Gan <iakov@amazon.fr>
2022-04-13 14:57:07 +0200

    add account_name

--- cid/builtin/core/data/queries/shared/business_units_map.sql
+++ cid/builtin/core/data/queries/shared/business_units_map.sql
@@ -3,6 +3,6 @@ SELECT *
 FROM
   (
  VALUES
-     ROW ('111111111', 'Business Unit 1')
-   , ROW ('222222222', 'Business Unit 1')
-)  ignored_table_name (account_id, bu)
+     ROW ('111111111', '111111111', 'Business Unit 1')
+   , ROW ('222222222', '111111111', 'Business Unit 1')
+)  ignored_table_name (account_id, account_name,bu)
