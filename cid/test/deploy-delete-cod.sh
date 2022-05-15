8ba44afda8dba9e8d7ba9637117adab586d4b530
Iakov Gan <iakov@amazon.fr>
2022-05-15 16:13:37 +0200

    fix lambda path

--- cid/test/deploy-delete-cod.sh
+++ cid/test/deploy-delete-cod.sh
@@ -2,7 +2,7 @@
 cid-cmd -vv deploy  \
    --dashboard-id compute-optimizer-dashboard \
    --athena-database optimization_data \
-   --view-compute-optimizer-lambda-lines-s3FolderPath       's3://costoptimizationdata{account_id}/Compute_Optimizer/Compute_Optimizer_ec2_lambda' \
+   --view-compute-optimizer-lambda-lines-s3FolderPath       's3://costoptimizationdata{account_id}/Compute_Optimizer/Compute_Optimizer_lambda' \
    --view-compute-optimizer-ebs-volume-lines-s3FolderPath   's3://costoptimizationdata{account_id}/Compute_Optimizer/Compute_Optimizer_ebs_volume' \
    --view-compute-optimizer-auto-scale-lines-s3FolderPath   's3://costoptimizationdata{account_id}/Compute_Optimizer/Compute_Optimizer_auto_scale' \
    --view-compute-optimizer-ec2-instance-lines-s3FolderPath 's3://costoptimizationdata{account_id}/Compute_Optimizer/Compute_Optimizer_ec2_instance'
