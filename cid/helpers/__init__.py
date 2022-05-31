105761caf1615587a38924e20e9871b14fd5d22a
Oleksandr Moskalenko <darken99@users.noreply.github.com>
2022-05-31 14:07:40 +0200

    Improve dataset creation

--- cid/helpers/__init__.py
+++ cid/helpers/__init__.py
@@ -1,7 +1,7 @@
 from cid.helpers.athena import Athena
 from cid.helpers.cur import CUR
 from cid.helpers.glue import Glue
-from cid.helpers.quicksight import QuickSight, Dashboard, Dataset
+from cid.helpers.quicksight import QuickSight, Dashboard, Dataset, Datasource
 
 
-__all__ = ["Athena", "CUR", "Glue", "QuickSight", "Dashboard", "Dataset"]
+__all__ = ["Athena", "CUR", "Glue", "QuickSight", "Dashboard", "Dataset", "Datasource"]
