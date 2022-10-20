562bf6a3a2c59952313a2282db0243202ebada52
Gabor Schulz <gaborsch@amazon.com>
2022-10-20 12:44:14 +0200

    Added init command to cli

--- cid/cli.py
+++ cid/cli.py
@@ -183,5 +183,11 @@ def share(ctx, dashboard_id, **kwargs):
     
     ctx.obj.share(dashboard_id)
 
+@cid_command
+def init(ctx, **kwargs):
+    """Initialize account resources for deployment"""
+    
+    ctx.obj.init(**kwargs)
+
 if __name__ == '__main__':
     main()
