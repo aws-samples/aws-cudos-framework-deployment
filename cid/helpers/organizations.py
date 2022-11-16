ea7899926da4c9cfb92e441c80e06051fa46315c
Gabor Schulz <gaborsch@amazon.com>
2022-11-16 14:47:53 +0100

    Code quality improvements

--- cid/helpers/organizations.py
+++ cid/helpers/organizations.py
@@ -10,21 +10,23 @@ class Organizations(CidBase):
     def __init__(self, session):
         super().__init__(session)
         self.client = self.session.client('organizations', region_name=self.region)
-    
+
     def get_account_email(self):
+        """ Try to extract the account's email address for Organizations """
         try:
             response = self.client.describe_account(AccountId=self.account_id)
             result = response.get('Email', '')
-        except Exception as ex:
+        except Exception:  # pylint: disable=broad-except
             result = ''
-            
+
         return result
-    
+
     def get_account_name(self):
+        """ Try to extract the account name from Organizations """
         try:
             response = self.client.describe_account(AccountId=self.account_id)
             result = response.get('Name', '')
-        except Exception as ex:
+        except Exception:  # pylint: disable=broad-except
             result = ''
-            
+
         return result
