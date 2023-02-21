37c41ec552df75aa9bb0f982b4a394fa60d00695
Babic <andbabic@b0be83585513.ant.amazon.com>
2023-02-21 10:05:31 +0100

    create account_map view as from template instead from python code

---
+++ cid/builtin/core/data/queries/shared/account_map_cfn.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW account_map AS
+SELECT DISTINCT
+    line_item_usage_account_id                                     account_id,
+    MAX_BY(line_item_usage_account_id, line_item_usage_start_date) account_name,
+    MAX_BY(bill_payer_account_id,      line_item_usage_start_date) parent_account_id,
+    CAST('' AS varchar)                                            account_status,
+    CAST('' AS varchar)                                            account_email,
+    CAST('' AS varchar)                                            account_email_id
+FROM
+    "${cur_table_name}"
+WHERE
+    line_item_usage_account_id <> ''
+GROUP BY
+    line_item_usage_account_id
