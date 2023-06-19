27dbc76fe3a759be409ec5b2647ad788e10494f7
Babic <andbabic@b0be83585513.ant.amazon.com>
2023-06-19 16:26:22 +0200

    bugfix when time_to is smaller than time_from, for example '21:00-01:01'

--- cid/helpers/randtime.py
+++ cid/helpers/randtime.py
@@ -14,6 +14,8 @@ def get_random_time_from_range(time_range):
         time_re = re.match(r'^(([01]\d|2[0-3]):([0-5]\d)|24:00)-(([01]\d|2[0-3]):([0-5]\d)|24:00)$', time_range)
         time_from = datetime.strptime(time_re.group(1), '%H:%M')
         time_to = datetime.strptime(time_re.group(4), '%H:%M')
+        if time_to < time_from:
+            time_to += timedelta(days=1)
         time_diff_sec = (time_to - time_from).total_seconds()
         random_seconds = random.randrange(0, int(time_diff_sec))
         random_time = (time_from + timedelta(seconds=random_seconds)).strftime('%H:%M')
