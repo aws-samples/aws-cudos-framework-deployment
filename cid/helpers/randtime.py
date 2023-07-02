05a650e5fae8df9005207e4de6f7f1a33e2ebcab
Iakov Gan <iakov@amazon.com>
2023-07-02 08:29:40 +0200

    linter for randtime

--- cid/helpers/randtime.py
+++ cid/helpers/randtime.py
@@ -1,36 +1,35 @@
-import re
+''' Helper functions for dataset schedules
+'''
 import hashlib
 from datetime import datetime, timedelta
 
-def random_generator(hashable_string, maximum_value):
-    """Provide random integer number based on input parameter"""
-    return int.from_bytes(bytes.fromhex(hashlib.md5(bytes(hashable_string, "utf-8")).hexdigest()[:16]), 'little', signed=True)%int(maximum_value)
+def pseudo_random_generator(hashable_string: str, maximum: int=100) -> int:
+    """Gernerate a pseudo random integer number, but the same for any given hashable_string identifier """
+    hash_hex = hashlib.md5(bytes(hashable_string, "utf-8"), usedforsecurity=False).hexdigest()[:16]
+    bigint_value = int.from_bytes(bytes.fromhex(hash_hex), 'little', signed=True)
+    return bigint_value % int(maximum)
 
-def get_random_time_from_range(hashable_string, input_time):
-    """
+def get_random_time_from_range(hashable_string, time_range):
+    """ Generate a random time from a given range
     In case that input time is in format hh:mm, just return it back.
     When input time is time range hh:mm-hh:mm, then return random time in format hh:mm within provided time range
     """
-    random_time = ""
+    items = time_range.strip().split('-')
 
-    timerange = input_time.strip().split('-')
-
-    if len(timerange) == 1:
+    if len(items) == 1:
         try:
-            random_time = datetime.strptime(input_time.strip(), '%H:%M').strftime('%H:%M')
+            return datetime.strptime(time_range.strip(), '%H:%M').strftime('%H:%M')
         except Exception as exc:
-            raise ValueError(f'Invalid time range "{input_time}": {str(exc)}')
-        return random_time
-    elif len(timerange) == 2:
+            raise ValueError(f'Invalid time range "{time_range}": {str(exc)}') from exc
+    elif len(items) == 2:
         try:
-            time_from = datetime.strptime(timerange[0].strip(), '%H:%M')
-            time_to = datetime.strptime(timerange[1].strip(), '%H:%M')
+            time_from = datetime.strptime(items[0].strip(), '%H:%M')
+            time_to = datetime.strptime(items[1].strip(), '%H:%M')
             if time_to < time_from:
                 time_to += timedelta(days=1)
             time_diff_sec = (time_to - time_from).total_seconds()
-            random_time = (time_from + timedelta(seconds=random_generator(hashable_string, time_diff_sec))).strftime('%H:%M')
+            return (time_from + timedelta(seconds=pseudo_random_generator(hashable_string, time_diff_sec))).strftime('%H:%M')
         except Exception as exc:
-            raise ValueError(f'Invalid time range "{input_time}": {str(exc)}')
-        return random_time
+            raise ValueError(f'Invalid time range "{time_range}": {str(exc)}') from exc
     else:
-        raise ValueError(f'Invalid time range "{input_time}". Please provide timerange in format hh:mm or hh:mm-hh:mm')
+        raise ValueError(f'Invalid time range "{time_range}". Please provide timerange in format hh:mm or hh:mm-hh:mm')
