54216eeafa350229ed66bcad7ccdfc71970a52a4
Iakov Gan <iakov@amazon.com>
2023-11-12 18:25:51 +0100

    avoid rebuilding zip

--- assets/build_lambda_layer.sh
+++ assets/build_lambda_layer.sh
@@ -3,6 +3,10 @@
 export CID_VERSION=$(python3 -c "from cid import _version;print(_version.__version__)")
 rm -rf build
 
+function get_hash {
+    find ./cid -type f -exec md5sum {} + | md5sum | awk '{print $1}'
+}
+
 function build_layer {
     echo 'Building a layer'
     mkdir -p ./python
@@ -12,8 +16,14 @@ function build_layer {
     rm -rf ./python
 }
 
-build_layer 1>&2
+# Check if code has been changed
+previous_hash=$(cat cid-$CID_VERSION.hash)
+actual_hash=$(get_hash)
+if [ "$actual_hash" == "$previous_hash" ] && [ -e "cid-$CID_VERSION.zip" ]; then
+    echo "No changes in code. Reuse existing zip." 1>&2
+else
+    build_layer 1>&2
+    echo $actual_hash > cid-$CID_VERSION.hash
+fi
 
 ls cid-$CID_VERSION.zip
-
-
