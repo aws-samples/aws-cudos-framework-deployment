6d28b7495955e63d433e45a0f8d6cba3198159a9
Iakov GAN <82834333+iakov-aws@users.noreply.github.com>
2023-11-20 13:10:58 +0100

    release 0.2.37 (#694)

--- bump-release.py
+++ bump-release.py
@@ -1,10 +1,13 @@
 import os
 import sys
+
+os.system('git checkout main')
+os.system('git pull')
+
 from cid._version import __version__ as old_ver
 
-
 bump='patch'
-if len(sys.argv)>1 :
+if len(sys.argv)>1:
 	bump = sys.argv[1]
 
 maj, minor, patch = map(int, old_ver.split('.'))
@@ -15,6 +18,8 @@ elif bump=='minor':
 	new_ver = '.'.join(map(str,[maj, minor + 1, 0]))
 else:
 	raise NotImplementedError('only patch and minor are implemented')
+
+os.system(f"git checkout -b 'release/{new_ver}'")
 
 
 tx = open('cid/_version.py').read()
@@ -28,3 +33,4 @@ with open('cfn-templates/cid-cfn.yml', "w") as f:
 
 os.system('git diff HEAD --unified=0')
 print('to undo:\n git checkout HEAD -- cfn-templates/cid-cfn.yml cid/_version.py')
+print(f"to continue:\n git commit -am 'release {new_ver}'; git push origin 'release/{new_ver}'")
