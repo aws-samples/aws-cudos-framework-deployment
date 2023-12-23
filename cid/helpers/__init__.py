6d8f31439e2d9ba82dd5e97a6c8f8115cd0ebe5e
Iakov Gan <iakov@amazon.com>
2023-12-23 23:28:13 +0100

    fix import

--- cid/helpers/__init__.py
+++ cid/helpers/__init__.py
@@ -1,6 +1,6 @@
 from cid.helpers.athena import Athena
-from cid.helpers.cur import CUR
 from cid.helpers.glue import Glue
+from cid.helpers.cur import CUR
 from cid.helpers.diff import diff
 from cid.helpers.quicksight import QuickSight, Dashboard, Dataset, Datasource, Template
 from cid.helpers.csv2view import csv2view
