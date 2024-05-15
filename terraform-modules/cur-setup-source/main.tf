88938fa940d63ec99b3d1c32b87257c7b4ee232a
Iakov GAN <82834333+iakov-aws@users.noreply.github.com>
2024-05-15 09:51:31 +0200

    add scad to cur (#794)
    * add scad to cur
    
    * fix SCAD
    
    * minor fixes
    
    * activate SPLIT_COST_ALLOCATION_DATA option in Terraform (#808)
    
    ---------
    
    Co-authored-by: Maxim Fedchishin <sevmax@gmail.com>

--- terraform-modules/cur-setup-source/main.tf
+++ terraform-modules/cur-setup-source/main.tf
@@ -240,7 +240,7 @@ resource "aws_cur_report_definition" "this" {
   time_unit                  = "HOURLY"
   format                     = "Parquet"
   compression                = "Parquet"
-  additional_schema_elements = ["RESOURCES"]
+  additional_schema_elements = var.enable_split_cost_allocation_data ? ["RESOURCES", "SPLIT_COST_ALLOCATION_DATA"] : ["RESOURCES"]
   s3_bucket                  = aws_s3_bucket.this.bucket
   s3_region                  = data.aws_region.this.name
   s3_prefix                  = "cur/${data.aws_caller_identity.this.account_id}"
