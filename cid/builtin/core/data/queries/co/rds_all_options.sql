73c0764476ecb2a58f3b6d8a69c6018565d5259c
Voicu Chirtes <voicuaws@gmail.com>
2024-07-09 22:37:06 -0700

    working options view for all rds combined storage and intance

--- cid/builtin/core/data/queries/co/rds_all_options.sql
+++ cid/builtin/core/data/queries/co/rds_all_options.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE VIEW "compute_optimizer_rds_all_options" AS
    , instancefinding finding
    , CONCAT((CASE WHEN (instancefindingreasoncodes_iscpuoverprovisioned = 'true') THEN 'CPU-Over ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_iscpuunderprovisioned = 'true') THEN 'CPU-Under ' ELSE '' END), (CASE WHEN (storagefindingreasoncodes_isebsvolumeiopsoverprovisioned = 'true') THEN 'DiskIOPS-Over ' ELSE '' END), (CASE WHEN (storagefindingreasoncodes_isebsvolumethroughputoverprovisioned = 'true') THEN 'DiskThroughput-Over ' ELSE '' END), (CASE WHEN (storagefindingreasoncodes_isebsvolumethroughputunderprovisioned = 'true') THEN 'DiskThroughput-Under ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_isebsiopsoverprovisioned = 'true') THEN 'EBSIOPS-Over ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_isebsthroughputoverprovisioned = 'true') THEN 'EBSThroughput-Over ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_isebsthroughputunderprovisioned = 'true') THEN 'EBSThroughput-Under ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_isnetworkbandwidthoverprovisioned = 'true') THEN 'NetworkBandwidth-Over ' ELSE '' END), (CASE WHEN (instancefindingreasoncodes_isnetworkbandwidthunderprovisioned = 'true') THEN 'NetworkBandwidth-Under ' ELSE '' END)) reason
    , lookbackperiodindays lookbackperiodindays
-   , '' currentperformancerisk
+   , 'none' currentperformancerisk
    , errorcode errorcode
    , errormessage errormessage
    , effectiverecommendationpreferencesenhancedinfrastructuremetrics ressouce_details
@@ -56,15 +56,16 @@ UNION    SELECT
    , currentdbinstanceclass option_from
    , instancerecommendationoptions_1_dbinstanceclass option_to
    , instanceRecommendationOptions_1_estimatedMonthlySavingsCurrency currency
-   --WIP COMPLETED UNTIL HERE
-   , TRY((CAST(recommendationoptions_1_ondemandprice AS double) * 730)) monthlyprice
-   , TRY(CAST(recommendationoptions_1_ondemandprice AS double)) hourlyprice
-   , TRY_CAST(recommendationoptions_1_estimatedmonthlysavings_value AS double) estimatedmonthlysavings_value
-   , TRY(((CAST(current_ondemandprice AS double) - CAST(recommendationoptions_1_ondemandprice AS double)) * 730)) estimatedmonthly_ondemand_cost_change
+   , TRY((CAST(instancerecommendationoptions_1_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_1_ondemandmonthlyprice AS double)) monthlyprice
+   , TRY(CAST(instancerecommendationoptions_1_instanceondemandhourlyprice AS double) + (CAST(storagerecommendationoptions_1_ondemandmonthlyprice AS double) / 730)) hourlyprice
+   , TRY(CAST(instancerecommendationoptions_1_instanceondemandhourlyprice AS double)) rdscurrentinstanceondemandhourlyprice
+   , TRY(CAST(storagerecommendationoptions_1_ondemandmonthlyprice AS double) / 730) rdscurrentstorageondemandhourlyprice
+   , TRY(CAST(instancerecommendationoptions_1_estimatedmonthlysavingsvalue AS double) + CAST(storagerecommendationoptions_1_estimatedmonthlysavingsvalue as double)) estimatedmonthlysavings_value
+   , TRY(((CAST(currentinstanceondemandhourlyprice AS double) * 730) + CAST(currentstorageondemandmonthlyprice AS double)) - ((CAST(instancerecommendationoptions_1_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_1_ondemandmonthlyprice AS double))) estimatedmonthly_ondemand_cost_change
    , 0E0 max_estimatedmonthlysavings_value_very_low
    , 0E0 max_estimatedmonthlysavings_value_low
    , 0E0 max_estimatedmonthlysavings_value_medium
-   , CONCAT(COALESCE(recommendationoptions_1_performancerisk, 'na'), ';', COALESCE(instancerecommendationoptions_1_dbinstanceclass, 'na'), ';', COALESCE(recommendationoptions_1_migrationeffort, 'na'), ';', COALESCE(recommendationoptions_1_memory, 'na'), ';', COALESCE(recommendationoptions_1_vcpus, 'na'), ';', COALESCE(recommendationoptions_1_network, 'na'), ';', COALESCE(recommendationoptions_1_storage, 'na'), ';', CONCAT((CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_isarchitecturedifferent, 'na') = 'true') THEN 'Architecture ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_ishypervisordifferent, 'na') = 'true') THEN 'Hypervisor ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_isinstancestoreavailabilitydifferent, 'na') = 'true') THEN 'InstanceStoreAvailability ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_isnetworkinterfacedifferent, 'na') = 'true') THEN 'NetworkInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_isstorageinterfacedifferent, 'na') = 'true') THEN 'StorageInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_1_platformdifferences_isvirtualizationtypedifferent, 'na') = 'true') THEN 'VirtualizationType ' ELSE '' END)), ';', COALESCE(recommendationoptions_1_projectedutilizationmetrics_cpu_maximum, 'na'), ';', COALESCE(recommendationoptions_1_projectedutilizationmetrics_memory_maximum, 'na'), ';') option_details
+   , CONCAT(COALESCE(instancerecommendationoptions_1_performancerisk, 'na'), ';', COALESCE(instancerecommendationoptions_1_dbinstanceclass, 'na'), ';', COALESCE(instancerecommendationoptions_1_rank, 'na'), ';', COALESCE(instancerecommendationoptions_1_projectedutilizationmetricscpumaximum, 'na'), ';', COALESCE(storagerecommendationoptions_1_storagetype, 'na'), ';', COALESCE(storagerecommendationoptions_1_allocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_1_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_1_iops, 'na'), ';', COALESCE(storagerecommendationoptions_1_storagethroughput, 'na'), ';', COALESCE(storagerecommendationoptions_1_rank, 'na'), ';', COALESCE(storagerecommendationoptions_1_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_1_iops, 'na'), ';') option_details
    , tags tags
    FROM
      compute_optimizer_rds_instance_lines
@@ -88,20 +89,22 @@ UNION    SELECT
    , '' utilizationmetrics
    , 'Option 2' option_name
    , currentdbinstanceclass option_from
-   , recommendationoptions_2_instancetype option_to
-   , recommendationoptions_2_estimatedmonthlysavings_currency currency
-   , TRY((CAST(recommendationoptions_2_ondemandprice AS double) * 730)) monthlyprice
-   , TRY(CAST(recommendationoptions_2_ondemandprice AS double)) hourlyprice
-   , TRY_CAST(recommendationoptions_2_estimatedmonthlysavings_value AS double) estimatedmonthlysavings_value
-   , TRY(((CAST(current_ondemandprice AS double) - CAST(recommendationoptions_2_ondemandprice AS double)) * 730)) estimatedmonthly_ondemand_cost_change
+   , instancerecommendationoptions_2_dbinstanceclass option_to
+   , instanceRecommendationOptions_2_estimatedMonthlySavingsCurrency currency
+   , TRY((CAST(instancerecommendationoptions_2_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_2_ondemandmonthlyprice AS double)) monthlyprice
+   , TRY(CAST(instancerecommendationoptions_2_instanceondemandhourlyprice AS double) + (CAST(storagerecommendationoptions_2_ondemandmonthlyprice AS double) / 730)) hourlyprice
+   , TRY(CAST(instancerecommendationoptions_2_instanceondemandhourlyprice AS double)) rdscurrentinstanceondemandhourlyprice
+   , TRY(CAST(storagerecommendationoptions_2_ondemandmonthlyprice AS double) / 730) rdscurrentstorageondemandhourlyprice
+   , TRY(CAST(instancerecommendationoptions_2_estimatedmonthlysavingsvalue AS double) + CAST(storagerecommendationoptions_2_estimatedmonthlysavingsvalue as double)) estimatedmonthlysavings_value
+   , TRY(((CAST(currentinstanceondemandhourlyprice AS double) * 730) + CAST(currentstorageondemandmonthlyprice AS double)) - ((CAST(instancerecommendationoptions_2_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_2_ondemandmonthlyprice AS double))) estimatedmonthly_ondemand_cost_change
    , 0E0 max_estimatedmonthlysavings_value_very_low
    , 0E0 max_estimatedmonthlysavings_value_low
    , 0E0 max_estimatedmonthlysavings_value_medium
-   , CONCAT(COALESCE(recommendationoptions_2_performancerisk, 'na'), ';', COALESCE(recommendationoptions_2_instancetype, 'na'), ';', COALESCE(recommendationoptions_2_migrationeffort, 'na'), ';', COALESCE(recommendationoptions_2_memory, 'na'), ';', COALESCE(recommendationoptions_2_vcpus, 'na'), ';', COALESCE(recommendationoptions_2_network, 'na'), ';', COALESCE(recommendationoptions_2_storage, 'na'), ';', CONCAT((CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_isarchitecturedifferent, 'na') = 'true') THEN 'Architecture ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_ishypervisordifferent, 'na') = 'true') THEN 'Hypervisor ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_isinstancestoreavailabilitydifferent, 'na') = 'true') THEN 'InstanceStoreAvailability ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_isnetworkinterfacedifferent, 'na') = 'true') THEN 'NetworkInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_isstorageinterfacedifferent, 'na') = 'true') THEN 'StorageInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_2_platformdifferences_isvirtualizationtypedifferent, 'na') = 'true') THEN 'VirtualizationType ' ELSE '' END)), ';', COALESCE(recommendationoptions_2_projectedutilizationmetrics_cpu_maximum, 'na'), ';', COALESCE(recommendationoptions_2_projectedutilizationmetrics_memory_maximum, 'na'), ';') option_details
+   , CONCAT(COALESCE(instancerecommendationoptions_2_performancerisk, 'na'), ';', COALESCE(instancerecommendationoptions_2_dbinstanceclass, 'na'), ';', COALESCE(instancerecommendationoptions_2_rank, 'na'), ';', COALESCE(instancerecommendationoptions_2_projectedutilizationmetricscpumaximum, 'na'), ';', COALESCE(storagerecommendationoptions_2_storagetype, 'na'), ';', COALESCE(storagerecommendationoptions_2_allocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_2_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_2_iops, 'na'), ';', COALESCE(storagerecommendationoptions_2_storagethroughput, 'na'), ';', COALESCE(storagerecommendationoptions_2_rank, 'na'), ';', COALESCE(storagerecommendationoptions_2_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_2_iops, 'na'), ';') option_details
    , tags tags
    FROM
      compute_optimizer_rds_instance_lines
-   WHERE ((resourcearn LIKE '%arn:%') AND (recommendationoptions_2_estimatedmonthlysavings_currency <> ''))
+   WHERE ((resourcearn LIKE '%arn:%') AND (instancerecommendationoptions_2_estimatedmonthlysavingscurrency <> ''))
 UNION    SELECT
      TRY("date_parse"(lastrefreshtimestamp, '%Y-%m-%d %H:%i:%s')) lastrefreshtimestamp
    , accountid accountid
@@ -121,18 +124,20 @@ UNION    SELECT
    , '' utilizationmetrics
    , 'Option 3' option_name
    , currentdbinstanceclass option_from
-   , recommendationoptions_3_instancetype option_to
-   , recommendationoptions_3_estimatedmonthlysavings_currency currency
-   , TRY((CAST(recommendationoptions_3_ondemandprice AS double) * 730)) monthlyprice
-   , TRY(CAST(recommendationoptions_3_ondemandprice AS double)) hourlyprice
-   , TRY_CAST(recommendationoptions_3_estimatedmonthlysavings_value AS double) estimatedmonthlysavings_value
-   , TRY(((CAST(current_ondemandprice AS double) - CAST(recommendationoptions_3_ondemandprice AS double)) * 730)) estimatedmonthly_ondemand_cost_change
+   , instancerecommendationoptions_3_dbinstanceclass option_to
+   , instanceRecommendationOptions_3_estimatedMonthlySavingsCurrency currency
+   , TRY((CAST(instancerecommendationoptions_3_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_3_ondemandmonthlyprice AS double)) monthlyprice
+   , TRY(CAST(instancerecommendationoptions_3_instanceondemandhourlyprice AS double) + (CAST(storagerecommendationoptions_3_ondemandmonthlyprice AS double) / 730)) hourlyprice
+   , TRY(CAST(instancerecommendationoptions_3_instanceondemandhourlyprice AS double)) rdscurrentinstanceondemandhourlyprice
+   , TRY(CAST(storagerecommendationoptions_3_ondemandmonthlyprice AS double) / 730) rdscurrentstorageondemandhourlyprice
+   , TRY(CAST(instancerecommendationoptions_3_estimatedmonthlysavingsvalue AS double) + CAST(storagerecommendationoptions_3_estimatedmonthlysavingsvalue as double)) estimatedmonthlysavings_value
+   , TRY(((CAST(currentinstanceondemandhourlyprice AS double) * 730) + CAST(currentstorageondemandmonthlyprice AS double)) - ((CAST(instancerecommendationoptions_3_instanceondemandhourlyprice AS double) * 730) + CAST(storagerecommendationoptions_3_ondemandmonthlyprice AS double))) estimatedmonthly_ondemand_cost_change
    , 0E0 max_estimatedmonthlysavings_value_very_low
    , 0E0 max_estimatedmonthlysavings_value_low
    , 0E0 max_estimatedmonthlysavings_value_medium
-   , CONCAT(COALESCE(recommendationoptions_3_performancerisk, 'na'), ';', COALESCE(recommendationoptions_3_instancetype, 'na'), ';', COALESCE(recommendationoptions_3_migrationeffort, 'na'), ';', COALESCE(recommendationoptions_3_memory, 'na'), ';', COALESCE(recommendationoptions_3_vcpus, 'na'), ';', COALESCE(recommendationoptions_3_network, 'na'), ';', COALESCE(recommendationoptions_3_storage, 'na'), ';', CONCAT((CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_isarchitecturedifferent, 'na') = 'true') THEN 'Architecture ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_ishypervisordifferent, 'na') = 'true') THEN 'Hypervisor ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_isinstancestoreavailabilitydifferent, 'na') = 'true') THEN 'InstanceStoreAvailability ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_isnetworkinterfacedifferent, 'na') = 'true') THEN 'NetworkInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_isstorageinterfacedifferent, 'na') = 'true') THEN 'StorageInterface ' ELSE '' END), (CASE WHEN (COALESCE(recommendationoptions_3_platformdifferences_isvirtualizationtypedifferent, 'na') = 'true') THEN 'VirtualizationType ' ELSE '' END)), ';', COALESCE(recommendationoptions_3_projectedutilizationmetrics_cpu_maximum, 'na'), ';', COALESCE(recommendationoptions_3_projectedutilizationmetrics_memory_maximum, 'na'), ';') option_details
+   , CONCAT(COALESCE(instancerecommendationoptions_3_performancerisk, 'na'), ';', COALESCE(instancerecommendationoptions_3_dbinstanceclass, 'na'), ';', COALESCE(instancerecommendationoptions_3_rank, 'na'), ';', COALESCE(instancerecommendationoptions_3_projectedutilizationmetricscpumaximum, 'na'), ';', COALESCE(storagerecommendationoptions_3_storagetype, 'na'), ';', COALESCE(storagerecommendationoptions_3_allocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_3_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_3_iops, 'na'), ';', COALESCE(storagerecommendationoptions_3_storagethroughput, 'na'), ';', COALESCE(storagerecommendationoptions_3_rank, 'na'), ';', COALESCE(storagerecommendationoptions_3_maxallocatedstorage, 'na'), ';', COALESCE(storagerecommendationoptions_3_iops, 'na'), ';') option_details
    , tags tags
    FROM
-     compute_optimizer_ec2_instance_lines
-   WHERE ((resourcearn LIKE '%arn:%') AND (recommendationoptions_3_estimatedmonthlysavings_currency <> ''))
+     compute_optimizer_rds_instance_lines
+   WHERE ((resourcearn LIKE '%arn:%') AND (instancerecommendationoptions_3_estimatedmonthlysavingscurrency <> ''))
 ) 
