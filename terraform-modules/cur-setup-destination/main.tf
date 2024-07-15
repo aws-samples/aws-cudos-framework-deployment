e950f6be1686b1b99372669f6dc7390edcedc3f3
Jan-Arve Nygård <jan-arve.nygard@norsk-tipping.no>
2024-07-15 14:27:47 +0200

    Changed to StringLike for conditions with wildcard for Get operations (#900)
    Thank you for contributing the fix!

--- terraform-modules/cur-setup-destination/main.tf
+++ terraform-modules/cur-setup-destination/main.tf
@@ -184,7 +184,7 @@ data "aws_iam_policy_document" "bucket_policy" {
         "${aws_s3_bucket.this.arn}/*",
       ]
       condition {
-        test     = "StringEquals"
+        test     = "StringLike"
         values   = ["arn:${data.aws_partition.this.partition}:cur:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:definition/*"]
         variable = "aws:SourceArn"
       }
