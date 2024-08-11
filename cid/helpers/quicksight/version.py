0aeee03a53c3e6c6c94774062bd5cae98432e666
Iakov Gan <iakov@amazon.com>
2024-08-11 18:27:39 +0200

    add test for versions

--- cid/helpers/quicksight/version.py
+++ cid/helpers/quicksight/version.py
@@ -1,14 +1,19 @@
+""" Semantic version of dashboards in CID
+"""
+
 import re
 import logging
 
 logger = logging.getLogger(__name__)
 
 class CidVersion:
-    
+    """ Semantic version of dashboards in CID
+    """
+
     def __init__(self, version):
-        
+
         if isinstance(version, __class__):
-            self.major, self.minor, self.build = version.major, version.minor, version.build 
+            self.major, self.minor, self.build = version.major, version.minor, version.build
         elif isinstance(version, str):
             self.major, self.minor, self.build = self._parse(version)
         else:
@@ -16,48 +21,53 @@ class CidVersion:
 
     def __str__(self):
         return f'v{self.major}.{self.minor}.{self.build}'
-    
-    def _parse(self, str_version):
-        
-        version_pattern = re.compile(r"^[v|V](?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<build>[0-9]+)$")
+
+    def _parse(self, str_version, default=(1, 0, 0)):
+        version_pattern = re.compile(r"^[v|V](?P<major>[0-9]+)\.(?P<minor>[0-9]+)(\.(?P<build>[0-9]+))?$")
         results = version_pattern.match(str_version)
-        
+
         if not results:
             logger.debug(f'Could not find version pattern in provided string: {str_version}')
-            major = 1
-            minor = 0
-            build = 0
+            major,minor,build = default
         else:
             major = int(results.group("major"))
-            minor = int(results.group("minor"))
-            build = int(results.group("build"))
-
+            minor = int(results.group("minor") or 0)
+            build = int(results.group("build") or 0)
         return major, minor, build
-    
+
     def compatible_versions(self, _version) -> bool:
-        """
-            Return True when both version are on the same major branch
-        """
-                    
+        """ Return True when both version are on the same major branch """
         return CidVersion(_version).major == self.major
-    
+
     def __lt__(self, _version):
-        return self._get_version_as_tuple() < CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() < CidVersion(_version).as_tuple()
 
     def __le__(self, _version):
-        return self._get_version_as_tuple() <= CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() <= CidVersion(_version).as_tuple()
 
     def __eq__(self, _version):
-        return self._get_version_as_tuple() == CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() == CidVersion(_version).as_tuple()
 
     def __ge__(self, _version):
-        return self._get_version_as_tuple() >= CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() >= CidVersion(_version).as_tuple()
 
     def __gt__(self, _version):
-        return self._get_version_as_tuple() > CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() > CidVersion(_version).as_tuple()
 
     def __ne__(self, _version):
-        return self._get_version_as_tuple() != CidVersion(_version)._get_version_as_tuple()
+        return self.as_tuple() != CidVersion(_version).as_tuple()
 
-    def _get_version_as_tuple(self) -> tuple:
-        return (self.major,self.minor,self.build)
+    def as_tuple(self) -> tuple:
+        """ return version as tuple """
+        return (self.major, self.minor, self.build)
+
+
+def test_versions():
+    """ basic tests for versions
+    """
+    assert CidVersion('v1.2').as_tuple() == (1, 2, 0)
+    assert CidVersion('v1.2.3').as_tuple() == (1, 2, 3)
+    assert CidVersion('v1.3.3') > CidVersion('V1.2.4')
+    assert CidVersion('v1.3.3') >= CidVersion('v1.3.3')
+    assert CidVersion(CidVersion('v1.2')).as_tuple() == (1, 2, 0)
+    assert str(CidVersion('v1.2')) == 'v1.2.0'
