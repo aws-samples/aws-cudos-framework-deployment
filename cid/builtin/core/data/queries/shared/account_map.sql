7dcffb80b81a34ed9d277fcfdfe3113b83e4c04e
Iakov Gan <iakov@amazon.com>
2024-08-12 11:52:26 +0200

    fix map update

--- cid/builtin/core/data/queries/shared/account_map.sql
+++ cid/builtin/core/data/queries/shared/account_map.sql
@@ -3,7 +3,7 @@ SELECT
     ${account_id} AS account_id,
     MAX_BY(${account_name}, ${account_id}) AS account_name
 FROM
-   "${metadata_database}"."${metadata_table_name}"
+   "${metadata_database_name}"."${metadata_table_name}"
 WHERE
    ${account_name} <> ''
 GROUP BY
