83eae05ae40796e5d168b15f7dc21131f13adef3
Iakov Gan <iakov@amazon.com>
2024-09-19 00:02:18 +0200

    revert to string

--- cid/builtin/core/data/queries/cid/ri_sp_mapping.sql
+++ cid/builtin/core/data/queries/cid/ri_sp_mapping.sql
@@ -13,7 +13,7 @@ FROM
      "bill_billing_period_start_date" "billing_period_mapping"
    , "bill_payer_account_id" "payer_account_id_mapping"
    , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN "savings_plan_savings_plan_a_r_n" WHEN ("reservation_reservation_a_r_n" <> '') THEN "reservation_reservation_a_r_n" ELSE ''   END) "ri_sp_arn_mapping"
-   , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN "savings_plan_end_time"           WHEN ("reservation_reservation_a_r_n" <> '') THEN "reservation_end_time"          ELSE null END) "ri_sp_end_date"
+   , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN CAST(CAST("from_iso8601_timestamp"("savings_plan_end_time") AS date) AS timestamp) WHEN (("reservation_reservation_a_r_n" <> '') AND ("reservation_end_time" <> '')) THEN CAST(CAST("from_iso8601_timestamp"("reservation_end_time") AS date) AS timestamp) ELSE null END) "ri_sp_end_date"
    , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN "savings_plan_purchase_term"      WHEN ("reservation_reservation_a_r_n" <> '') THEN "pricing_lease_contract_length" ELSE ''   END) "ri_sp_term"
    , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN "savings_plan_offering_type"      WHEN ("reservation_reservation_a_r_n" <> '') THEN "pricing_offering_class"        ELSE ''   END) "ri_sp_offering"
    , (CASE WHEN ("savings_plan_savings_plan_a_r_n" <> '') THEN "savings_plan_payment_option"     WHEN ("reservation_reservation_a_r_n" <> '') THEN "pricing_purchase_option"       ELSE ''   END) "ri_sp_payment"
