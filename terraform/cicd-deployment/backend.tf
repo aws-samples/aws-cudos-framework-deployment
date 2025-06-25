302bee3d2174c5698a298a2da7e4eea8e6d15a6c
Vineeth Nair <vtnair@amazon.co.uk>
2025-06-25 10:43:55 +0100

    backend default region updated to deploy in london region

--- terraform/cicd-deployment/backend.tf
+++ terraform/cicd-deployment/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket       = "your-terraform-state-bucket"
     key          = "cid-dashboard/terraform.tfstate"
-    region       = "us-east-1" # Replace with your desired region
+    region       = "eu-west-2" # Replace with your desired region
     use_lockfile = true        # terraform-state-lock
     encrypt      = true
   }
